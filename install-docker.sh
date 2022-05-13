80d0907b39627dedb7094cad1094d5224d554e64
Takane42 <dragster1616@gmail.com>
2022-05-13 16:14:25 +0700

    add some sudo cause i forgot to do it xd

--- install-docker.sh
+++ install-docker.sh
@@ -12,8 +12,8 @@ function install_docker() {
       sudo apt remove docker docker-engine docker.io containerd runc -y
       sudo apt update
       sudo apt install ca-certificates gnupg lsb-release -y
-      sudo curl -fsSL https://download.docker.com/linux/$DISTRO/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-      sudo echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/$DISTRO $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+      sudo curl -fsSL https://download.docker.com/linux/$DISTRO/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+      sudo echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/$DISTRO $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
       sudo apt-get update
       sudo apt-get install docker-ce docker-ce-cli containerd.io -y
       ;;
