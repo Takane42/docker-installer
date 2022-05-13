f8c349d58cd20e621987f2cb333b3f76ff24c18e
Takane42 <dragster1616@gmail.com>
2022-05-13 10:23:47 +0700

    add script usage and sudo for convenient use

--- install-docker.sh
+++ install-docker.sh
@@ -3,25 +3,26 @@
 #requirement
 # ubuntu / debian = sudo apt install curl
 # rhel / centos = sudo yum install curl
+# How To Use => curl -sL "https://bit.ly/DockerNata | sudo bash -s <ubuntu/debian/centos> <-wdc/--docker-compose/-dco/--compose-only> <1/2>"
 
 function install_docker() {
   case $DISTRO in
     ubuntu | debian)
       PACKAGE="apt"
-      apt remove docker docker-engine docker.io containerd runc -y
-      apt update
-      apt install ca-certificates gnupg lsb-release -y
-      curl -fsSL https://download.docker.com/linux/$DISTRO/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-      echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/$DISTRO $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-      apt-get update
-      apt-get install docker-ce docker-ce-cli containerd.io -y
+      sudo apt remove docker docker-engine docker.io containerd runc -y
+      sudo apt update
+      sudo apt install ca-certificates gnupg lsb-release -y
+      sudo curl -fsSL https://download.docker.com/linux/$DISTRO/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+      sudo echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/$DISTRO $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+      sudo apt-get update
+      sudo apt-get install docker-ce docker-ce-cli containerd.io -y
       ;;
     centos | rhel)
       PACKAGE="yum"
-      yum remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine -y
-      yum install -y yum-utils
-      yum-config-manager --add-repo https://download.docker.com/linux/$DISTRO/docker-ce.repo
-      yum install docker-ce docker-ce-cli containerd.io -y
+      sudo yum remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine -y
+      sudo yum install -y yum-utils
+      sudo yum-config-manager --add-repo https://download.docker.com/linux/$DISTRO/docker-ce.repo
+      sudo yum install docker-ce docker-ce-cli containerd.io -y
       ;;
   esac
 }
@@ -29,11 +30,11 @@ function install_docker() {
 function docker_compose() {
   case "$1" in
     1)
-      $PACKAGE install docker-compose-plugin -y ;;
+      sudo $PACKAGE install docker-compose-plugin -y ;;
     2)
-      rm -f /usr/local/bin/docker-compose
-      curl -SL $(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep -o -m 1 "https.*linux-x86_64") -o /usr/local/bin/docker-compose
-      chmod +x /usr/local/bin/docker-compose ;;
+      sudo rm -f /usr/local/bin/docker-compose
+      sudo curl -SL "$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep -o -m 1 "https.*linux-x86_64")" -o /usr/local/bin/docker-compose
+      sudo chmod +x /usr/local/bin/docker-compose ;;
     0)
       exit 0;;
     *)
@@ -41,7 +42,7 @@ function docker_compose() {
       echo " 1) via package-manager"
       echo " 2) standalone binary"
       echo " 0) exit"
-      read -p "Option : " OPTION
+      read -r -p "Option : " OPTION
       docker_compose $OPTION
   esac
 }
