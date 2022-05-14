9df25fa5f6279b3ca062975422f3164559d3bf36
Takane42 <dragster1616@gmail.com>
2022-05-14 12:07:26 +0700

    add temporary if else statement. you get the idea from the comment

--- install-docker.sh
+++ install-docker.sh
@@ -15,7 +15,7 @@ echo "======================="
       sudo apt remove docker docker-engine docker.io containerd runc -y
       sudo apt update
       sudo apt install ca-certificates gnupg lsb-release -y
-      sudo curl -fsSL https://download.docker.com/linux/$DISTRO/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+      sudo curl -fsSL https://download.docker.com/linux/"$DISTRO"/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
       sudo echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/$DISTRO $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
       sudo apt-get update
       sudo apt-get install docker-ce docker-ce-cli containerd.io -y
@@ -24,22 +24,19 @@ echo "======================="
       PACKAGE="yum"
       sudo yum remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine -y
       sudo yum install -y yum-utils
-      sudo yum-config-manager --add-repo https://download.docker.com/linux/$DISTRO/docker-ce.repo
+      sudo yum-config-manager --add-repo https://download.docker.com/linux/"$DISTRO"/docker-ce.repo
       sudo yum install docker-ce docker-ce-cli containerd.io -y
       ;;
     fedora)
       PACKAGE="dnf"
       sudo dnf remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine docker-engine-selinux docker-selinux -y
       sudo dnf -y install dnf-plugins-core
-      sudo dnf-config-manager --add-repo https://download.docker.com/linux/$DISTRO/docker-ce.repo
+      sudo dnf-config-manager --add-repo https://download.docker.com/linux/"$DISTRO"/docker-ce.repo
       sudo dnf install docker-ce docker-ce-cli containerd.io -y
   esac
 }
 
 docker_compose() {
-echo "================================"
-echo "2. Installing Docker-Compose"
-echo "================================"
   case "$1" in
     1)
       sudo $PACKAGE install docker-compose-plugin -y ;;
@@ -50,12 +47,15 @@ echo "================================"
     0)
       exit 0;;
     *)
+      echo "================================"
+      echo "2. Installing Docker-Compose"
+      echo "================================"
       echo "Please Choose the correct option"
       echo " 1) via package-manager"
       echo " 2) standalone binary"
       echo " 0) exit"
       read -r -p "Option : " OPTION
-      docker_compose $OPTION
+      docker_compose "$OPTION"
   esac
 }
 
@@ -67,10 +67,13 @@ if [[ -z $DISTRO ||  $DISTRO == *"-"* ]] ; then
     exit
 fi
 
-if [ "$(sudo systemctl is-active docker)" == "active" ] ; then
+# Need 3 arguments but get 2 cause idk how to use bash in this case :/
+# if docker exist and docker-compose flag is set. need to skip this check ( -dco and --compose-only )
+
+if [ "$(sudo systemctl is-active docker)" == "active" ] && [ ! "$2" == "-dco" ]; then
     echo "Docker is Already Installed"
-    read -r -p "Continue? (Y/n) : " docker_option
-    if [ $docker_option == n ] ; then
+    read -r -p "Reinstall Docker? (Y/n) : " docker_option
+    if [ "$docker_option" == n ] ; then
       echo "Install Canceled";
       exit 1
     fi
@@ -79,10 +82,10 @@ fi
 case "$2" in #$2 -> Flags
   -wdc|--docker-compose)
     install_docker "$DISTRO"
-    docker_compose $3
+    docker_compose "$3"
     ;;
   -dco|--compose-only)
-    docker_compose $3
+    docker_compose "$3"
     ;;
   *)
     install_docker "$DISTRO"
